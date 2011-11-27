a6332758b91a45f2565b5d4195ca691cc5b7b118
Christian Moscardi <clm2183@columbia.edu>
2011-11-27 12:57:14 -0500

    ported code from horstmann

---
+++ src/scene1/SelectableShape.java
@@ -0,0 +1,21 @@
+package scene1;
+import java.awt.*;
+import java.awt.geom.*;
+
+/**
+   A shape that manages its selection state.
+*/
+public abstract class SelectableShape implements SceneShape
+{
+   public void setSelected(boolean b)
+   {
+      selected = b;
+   }
+
+   public boolean isSelected()
+   {
+      return selected;
+   }
+
+   private boolean selected;
+}
